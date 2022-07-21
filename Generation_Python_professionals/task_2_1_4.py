3df5322c3153250f0aa61a10810c070b2f5049e9
Evgenii Zalesski <evgenii889111@gmail.com>
2022-07-21 12:00:14 +0300

    task 2.1.4 Функция print_given() Реализуйте функцию print_given(), которая принимает произвольное количество позиционных и именованных аргументов и выводит все переданные аргументы, указывая тип каждого. Пары аргумент-тип должны выводиться каждая на отдельной строке, в следующем формате:
    для позиционных аргументов:
    <значение аргумента> <тип аргумента>
    для именованных аргументов:
    <имя переменной> <значение аргумента> <тип аргумента>
    Примечание 1. При выводе позиционные аргументы должны быть расположены в порядке их передачи, именованные — в алфавитном порядке имен переменных.
    
    Примечание 2. При выводе сначала должны следовать все позиционные аргументы, затем — все именованные.
    
    Примечание 3. Если в функцию ничего не передается, функция ничего не должна выводить.

---
+++ Generation_Python_professionals/task_2_1_4.py
@@ -0,0 +1,11 @@
+def print_given(*args, **kwargs):
+    for elm in args:
+        print(f'{elm} {type(elm)}')
+    keys = sorted(kwargs)
+    for k in keys:
+        print(f'{k} {kwargs[k]} {type(kwargs[k])}')
+
+
+if __name__ == '__main__':
+    print_given(1, [1, 2, 3], 'three', two=2)
+    print_given('apple', 'cherry', 'watermelon')
