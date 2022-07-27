0033312447f07f8431e30aad47cc17d0cd488332
Evgenii Zalesski <evgenii889111@gmail.com>
2022-07-27 14:41:31 +0300

    task 3.2.6 В переменной florida_hurricane_dates хранится список дат, в которые произошел ураган во Флориде с 19501950 по 20172017 год.
    Дополните приведенный ниже код, чтобы он вывел самую раннюю дату из списка florida_hurriance_dates в трех различных форматах:
    
    в стандарте ISO (YYYY-MM-DD)
    в типичном для России стиле (DD.MM.YYYY)
    в типичном для Америки стиле (MM/DD/YYYY)

--- Generation_Python_professionals/task_3_2_6.py
+++ Generation_Python_professionals/task_3_2_6.py
@@ -3,8 +3,8 @@ first_date = min(florida_hurricane_dates)
 
 # конвертируем дату в ISO и RU формат
 iso = 'Дата первого урагана в ISO формате: ' + first_date.isoformat()
-ru = 'Дата первого урагана в RU формате: ' + first_date.se('____')
-us = 'Дата первого урагана в US формате: ' + first_date.____('____')
+ru = 'Дата первого урагана в RU формате: ' + first_date.strftime('%d.%m.%Y')
+us = 'Дата первого урагана в US формате: ' + first_date.strftime('%m/5d/%Y')
 
 print(iso)
 print(ru)
