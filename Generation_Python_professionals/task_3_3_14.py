e508b1d9ea0fdea05db48ad16625e503e9a81b89
Evgenii Zalesski <evgenii889111@gmail.com>
2022-07-29 13:24:00 +0300

    task 3.4.14 Вам доступен список times_of_purchases, содержащий даты (тип datetime), в которые были совершены покупки в некотором интернет-магазине. Дополните приведенный ниже код, чтобы он вывел текст До полудня, если большее число покупок было совершено до полудня, или текст После полудня в противном случае.
    Примечание 1. Гарантируется, что ни одна покупка не была совершена ровно в 12:00:00.
    
    Примечание 2. Гарантируется, что до полудня и после полудня совершено различное количество покупок.

---
+++ Generation_Python_professionals/task_3_3_14.py
@@ -0,0 +1,23 @@
+from datetime import datetime, time
+
+
+if __name__ == '__main__':
+    times_of_purchases = [datetime(2017, 10, 1, 12, 23, 25), datetime(2017, 10, 1, 15, 26, 26),
+                      datetime(2017, 10, 1, 15, 42, 57), datetime(2017, 10, 1, 17, 49, 59),
+                      datetime(2017, 10, 2, 6, 37, 10), datetime(2017, 10, 2, 6, 42, 53),
+                      datetime(2017, 10, 2, 8, 56, 45), datetime(2017, 10, 2, 9, 18, 3),
+                      datetime(2017, 10, 2, 12, 23, 48), datetime(2017, 10, 2, 12, 45, 5),
+                      datetime(2017, 10, 2, 12, 48, 8), datetime(2017, 10, 2, 12, 10, 54),
+                      datetime(2017, 10, 2, 19, 18, 10), datetime(2017, 10, 2, 12, 31, 45),
+                      datetime(2017, 10, 3, 20, 57, 10), datetime(2017, 10, 4, 7, 4, 57),
+                      datetime(2017, 10, 4, 7, 13, 31), datetime(2017, 10, 4, 7, 13, 42),
+                      datetime(2017, 10, 4, 7, 21, 54), datetime(2017, 10, 4, 14, 22, 12),
+                      datetime(2017, 10, 4, 14, 50), datetime(2017, 10, 4, 15, 7, 27),
+                      datetime(2017, 10, 4, 12, 44, 49), datetime(2017, 10, 4, 12, 46, 41),
+                      datetime(2017, 10, 4, 16, 32, 33),datetime(2017, 10, 4, 16, 34, 44),
+                      datetime(2017, 10, 4, 16, 46, 59), datetime(2017, 10, 4, 12, 26, 6)]
+    my_times = [my_date for my_date in times_of_purchases if time(my_date.hour, my_date.minute, my_date.second) < time(hour=12)]
+    if len(my_times) * 2 > len(times_of_purchases):
+        print('До полудня')
+    else:
+        print('После полудня')
