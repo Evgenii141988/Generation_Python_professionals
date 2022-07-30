37148498ef437dd282e39c2c52a405eedfb6e739
Evgenii Zalesski <evgenii889111@gmail.com>
2022-07-30 12:11:39 +0300

    Revert "task 3.4.12"
    This reverts commit 041221808809c44f40adc8d526088c68d2acb9a2.

--- Generation_Python_professionals/task_3_4_12.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-
-if __name__ == '__main__':
-    dt = datetime(2021, 11, 4, 13, 6) + timedelta(weeks=1, hours=12)
-
-    print(dt.strftime('%d.%m.%Y %H:%M:%S'))
