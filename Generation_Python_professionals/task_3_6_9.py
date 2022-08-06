f53e1b014ba073915fd782f24d5fcddea78d0aa6
Evgenii Zalesski <evgenii889111@gmail.com>
2022-08-06 10:33:26 +0300

    task 3.6.10-11 Вам доступны три реализации функции, которая вычисляет факториал числа nn:
    встроенная из модуля math
    рекурсивная
    итеративная
    Выясните, какая функция быстрее вычислит факториал числа 900.
    Вам доступны две реализации функции, которая создает и возвращает список из чисел от 11 до 10 \, 000 \, 00010000000 включительно:
    
    с использованием цикла for и метода append()
    с использованием списочного выражения
    Определите, какая функция быстрее создаст и вернет требуемый список.

--- Generation_Python_professionals/task_3_6_9.py
+++ Generation_Python_professionals/task_3_6_9.py
@@ -1,4 +1,5 @@
 import time
+from math import factorial  # функция из модуля math
 
 
 def get_the_fastest_func(funcs: list, arg):
@@ -15,4 +16,32 @@ def get_the_fastest_func(funcs: list, arg):
     return result_func
 
 
+def factorial_recurrent(n):  # рекурсивная функция
+    if n == 0:
+        return 1
+    return n * factorial_recurrent(n - 1)
 
+
+def factorial_classic(n):  # итеративная функция
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+    return f
+
+
+def for_and_append(iterations):  # с использованием цикла for и метода append()
+    result = []
+    for i in range(iterations):
+        result.append(i + 1)
+    return result
+
+
+def list_comprehension(iterations):  # с использованием списочного выражения
+    return [i + 1 for i in range(iterations)]
+
+
+if __name__ == '__main__':
+    func_list = [factorial, factorial_classic, factorial_recurrent]
+    print(get_the_fastest_func(func_list, 900))
+    funcs = [for_and_append, list_comprehension]
+    print(get_the_fastest_func(funcs, 10_000_000))
