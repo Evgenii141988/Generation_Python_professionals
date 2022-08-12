c8c843c914ad3b25189f778da52cf01e8f1084b6
Evgenii Zalesski <evgenii889111@gmail.com>
2022-08-12 21:52:33 +0300

    task 4.2.10 При попытке выполнить приведенную ниже программу возникает ошибка. Найдите и исправьте ее, чтобы программа создала файл writing_test.csv, имеющий следующее содержание:
    first_col,second_col
    value1,value2

---
+++ Generation_Python_professionals/task_4_2_10.py
@@ -0,0 +1,11 @@
+import csv
+
+
+if __name__ == '__main__':
+    with open('writing_test.csv', 'w', encoding='utf-8', newline='') as csv_file:
+        # создаем writer объект и указываем названия столбцов
+        writer = csv.DictWriter(csv_file, fieldnames=['first_col', 'second_col'])
+        # записываем первую строку с названиями столбцов
+        writer.writeheader()
+        # записываем строку с данными
+        writer.writerow({'first_col': 'value1', 'second_col': 'value2'})
