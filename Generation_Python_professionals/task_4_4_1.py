9c1594de0d01261c7f8498d8ac3c478a6c705a37
Evgenii Zalesski <evgenii889111@gmail.com>
2022-08-19 19:43:42 +0300

    task 4.4.1 Дополните приведенный ниже код, чтобы он вывел содержимое словаря countries, расположив его элементы в лексикографическом порядке ключей, указав в качестве разделителя пар ключ-значение строку   -  (пробел дефис пробел), а в качестве отступов — три пробела.
    Примечание 1. Начальная часть ответа выглядит так:
    
    {
       "Angola" - "Luanda",
       "Australia" - "Canberra",
       ...
    Примечание 2. Используйте необязательные аргументы indent, separators и sort_keys.

--- Generation_Python_professionals/task_4_4_1.py
+++ Generation_Python_professionals/task_4_4_1.py
@@ -5,6 +5,5 @@ if __name__ == '__main__':
                  'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
                  'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
                  'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
-
     countries_json = json.dumps(countries, indent='   ', sort_keys=True, separators=(',', ' - '))
     print(countries_json)
