6f219dfc563b05a86baf5cedf4cceb15745bc8f8
Evgenii Zalesski <evgenii889111@gmail.com>
2022-08-19 20:03:35 +0300

    task 4.4.3 Вам доступны словари club1, club2 и club3, содержащие данные о различных футбольных клубах. Дополните приведенный ниже код, чтобы он объединил данные словари в список и записал полученную структуру данных в файл data.json, указав в качестве отступов три символа пробела.
    Примечание 1. Словари в списке должны располагаться в своем исходном порядке.
    
    Примечание 2. Начальная часть файла data.json выглядит так:
    
    [
       {
          "name": "FC Byern Munchen",
          "country": "Germany",
          ...
       },
       ...
    ]
    Примечание 3. Используйте необязательный аргумент indent.

---
+++ Generation_Python_professionals/task_4_4_3.py
@@ -0,0 +1,16 @@
+import json
+if __name__ == '__main__':
+
+    club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
+             "trainer": "Julian Nagelsmann", "gaolkeeper": "M. Neuer", "league_position": 1}
+
+    club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
+             "trainer": "Xavier Creus", "gaolkeeper": "M. Ter Stegen", "league_position": 7}
+
+    club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
+             "trainer": "Michael Carrick", "gaolkeeper": "D. De Gea", "league_position": 8}
+
+    # clubs = [json.dumps(club, indent='   ') for club in (club1, club2, club3)]
+    clubs = [club1, club2, club3]
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(clubs, file, indent='   ')
