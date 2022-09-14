8ff4af022e537aeaa50ac237e4921b9191b78e9f
Evgenii Zalesski <evgenii889111@gmail.com>
2022-09-14 16:41:23 +0300

    task 6.4.9 Вам доступен именованный кортеж Game. Дополните приведенный ниже код, чтобы он создал именованный кортеж типа ExtendedGame, имеющий те же поля, что и Game, а также два дополнительных поля — release_date и price.

--- Generation_Python_professionals/task_6_4_9.py
+++ Generation_Python_professionals/task_6_4_9.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
+if __name__ == '__main__':
+    Game = namedtuple('Game', 'name developer publisher')
 
-Game = namedtuple('Game', 'name developer publisher')
-
-ExtendedGame = namedtuple('ExtendedGame', [*Game._fields, 'release_date', 'price'])
+    ExtendedGame = namedtuple('ExtendedGame', [*Game._fields, 'release_date', 'price'])
