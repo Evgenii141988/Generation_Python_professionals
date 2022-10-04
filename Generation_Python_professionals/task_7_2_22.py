522a58ab0a1afd8d98a3e7905bea63fd0daa04ab
Evgenii Zalesski <evgenii889111@gmail.com>
2022-10-04 18:04:42 +0300

    task 7.2.22 Вам доступна программа, которая добавляет в список remainders остаток от деления 3636 на каждое число из списка numbers. Если число равно нулю, оно игнорируется. Дополните приведенный ниже код конструкцией try-except, чтобы он выполнился без ошибок.

---
+++ Generation_Python_professionals/task_7_2_22.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    numbers = [6, 0, 36, 8, 2, 36, 0, 12, 60, 0, 45, 0, 3, 23]
+
+    remainders = []
+
+    for number in numbers:
+        try:
+            remainders.append(36 % number)
+        except ZeroDivisionError:
+            pass
+    print(remainders)
