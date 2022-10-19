402398da4f369a403d5dbda4d2e56aa9e0dd9e59
Evgenii Zalesski <evgenii889111@gmail.com>
2022-10-19 20:06:21 +0300

    task 8.3.15 Функция to_binary() Реализуйте функцию to_binary() с использованием рекурсии, которая принимает один аргумент:
    number — неотрицательное целое число
    Функция должна возвращать строковое представление числа number в двоичной системе счисления.

---
+++ Generation_Python_professionals/task_8_3_15.py
@@ -0,0 +1,17 @@
+def to_binary(number: int) -> str:
+    if number == 0:
+        return '0'
+    if number < 2:
+        return '1'
+    else:
+        return to_binary(number // 2) + str(number % 2)
+
+
+if __name__ == '__main__':
+    print(to_binary(15))
+    print(to_binary(0))
+    print(to_binary(1))
+    print(to_binary(2))
+    print(to_binary(3))
+    print(to_binary(12))
+    print(to_binary(5))
