beb58b71fbebd6414575864b832a4d63243d6659
Evgenii Zalesski <evgenii889111@gmail.com>
2022-10-24 23:22:50 +0300

    task 8.4.7 Функция dict_travel() 🌶️🌶️ Реализуйте функцию dict_travel(), которая принимает один аргумент:
    nested_dicts — словарь, содержащий в качестве значений числа, строки или словари, которые, в свою очередь, так же содержат в качестве значений числа, строки или словари; вложенность может быть произвольной
    Функция должна выводить все пары ключ-значение словаря nested_dicts, а также значения всех его дочерних словарей. При выводе значений дочерних словарей необходимо перечислять имена всех ключей, начиная с верхнего уровня, разделяя их точками.
    
    Например, в словаре:
    
    {'name': 'Arthur', 'grades': {'math': [4, 4], 'chemistry': [3, 4]}}
    значение [4, 4] должно быть выведено в следующем формате:
    
    grades.math: [4, 4]
    Все пары ключ-значение должны быть расположены в лексикографическом порядке, каждая на отдельной строке.
    
    Примечание 1. Гарантируется, что ключами в подаваемом в функцию словаре являются строки, содержащие только латинские буквы в нижнем регистре.
    
    Примечание 2. Гарантируется, что ни один ключ в подаваемом в функцию словаре не является последовательностью других ключей. Другими словами, словарь не может иметь, например, следующий вид:
    
    {'b.c': 1, 'b': {'c': 30, 'a': 10, 'b': 20}}

---
+++ Generation_Python_professionals/task_8_4_7.py
@@ -0,0 +1,51 @@
+def dict_travel(nested_dicts: dict):
+
+    def rec(dicts, string=''):
+        results = []
+        for k, v in dicts.items():
+            if type(v) != dict:
+                results.append(string + f'{k}: {v}')
+            else:
+                string += f'{k}.'
+                results.extend(rec(v, string))
+                string = '.'.join(string.split('.')[:-2]) + '.' if len(string.split('.')) > 2 else ''
+        return results
+    res = rec(nested_dicts)
+    print(*sorted(res), sep='\n')
+
+# def dict_travel(nested_dicts: dict, string=''):
+#     results = []
+#     for k, v in nested_dicts.items():
+#         if type(v) != dict:
+#             results.append(string + f'{k}: {v}')
+#         else:
+#             string += f'{k}.'
+#             results.extend(dict_travel(v, string))
+#     return results
+
+
+if __name__ == '__main__':
+    data = {'a': 1, 'b': {'c': 30, 'a': 10, 'b': 20}}
+
+    dict_travel(data)
+    # print(dict_travel(data))
+    print()
+
+    data = {'d': 1, 'b': {'c': 30, 'a': 10, 'b': 20}, 'a': 100}
+
+    dict_travel(data)
+    print()
+    # print(dict_travel(data))
+
+    data = {'b': {'c': 30, 'a': 10, 'b': {'d': 40, 'e': 50}}}
+
+    dict_travel(data)
+    # print(dict_travel(data))
+    print()
+
+    data = {'firstname': 'Тимур', 'lastname': 'Гуев', 'birthdate': {'day': 10, 'month': 'October', 'year': 1993},
+            'address': {'streetaddress': 'Часовая 25, кв. 127',
+                        'city': {'region': 'Московская область', 'type': 'город', 'cityname': 'Москва'},
+                        'postalcode': '125315'}}
+
+    dict_travel(data)
