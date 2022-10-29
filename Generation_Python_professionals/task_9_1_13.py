7e2a373d3cfd2c617b8f155f3d1448fa303c8947
Evgenii Zalesski <evgenii889111@gmail.com>
2022-10-29 19:06:25 +0300

    task 9.1.13 Необычная сортировка 🌶️ Дана строка, содержащая латинские буквы и цифры. Напишите программу, которая сортирует символы в строке согласно следующим правилам:
    все отсортированные строчные буквы стоят перед заглавными буквами
    все отсортированные заглавные буквы стоят перед цифрами
    все отсортированные нечетные цифры стоят перед четными
    Формат входных данных
    На вход программе подается строка, содержащая латинские буквы и цифры.

--- Generation_Python_professionals/task_9_1_13.py
+++ Generation_Python_professionals/task_9_1_13.py
@@ -6,8 +6,19 @@ def get_sorted_strings(string: str) -> str:
     return ''.join(lower_lit + upper_lit + odd_num + even_num)
 
 
+def comparator(char):
+    if char.isalpha():
+        return 0, char.isupper(), char
+    digit = int(char)
+    return 1, digit % 2 == 0, digit
+
+
 if __name__ == '__main__':
     print(get_sorted_strings(input()))
     print(get_sorted_strings('Sorting1234'))
     print(get_sorted_strings('n0tEast3rEgg'))
     print(get_sorted_strings('3DYrz34UXl'))
+
+    string = input()
+    print(sorted(string, key=comparator))
+    print(''.join(sorted(string, key=comparator)))
