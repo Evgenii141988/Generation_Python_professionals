d51e685c79eca3ef2e9c9aad1d4d25e7ade82324
Evgenii Zalesski <evgenii889111@gmail.com>
2022-11-09 20:45:56 +0300

    task 9.8.10 Декоратор trace Реализуйте декоратор trace, который выводит отладочную информацию о декорируемой функции во время ее выполнения, а именно: имя функции, переданные аргументы и возвращаемое значение в следующем формате:
    TRACE: вызов <имя функции> с аргументами: <кортеж позиционных аргументов>, <словарь именованных аргументов>
    TRACE: возвращаемое значение <имя функции>: <возвращаемое значение>
    Также декоратор должен сохранять имя и строку документации декорируемой функции.

---
+++ Generation_Python_professionals/task_9_8_10.py
@@ -0,0 +1,48 @@
+from functools import wraps
+
+
+def trace(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result_func = f"'{func(*args, **kwargs)}'" if type(func(*args, **kwargs)) == str else func(*args, **kwargs)
+        print(f"TRACE: вызов {func.__name__}() с аргументами: {args}, {kwargs}")
+        print(f"TRACE: возвращаемое значение {func.__name__}(): {result_func}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@trace
+def say(name, line):
+    return f'{name}: {line}'
+
+
+@trace
+def sub(a, b, c):
+    '''прекрасная функция'''
+    return a - b + c
+
+
+@trace
+def beegeek():
+    '''beegeek docs'''
+    return 'beegeek'
+
+
+if __name__ == '__main__':
+    say('Jane', 'Hello, World')
+
+    print(sub.__name__)
+    print(sub.__doc__)
+    sub(20, 5, c=10)
+
+
+    @trace
+    def beegeek():
+        '''beegeek docs'''
+        return 'beegeek'
+
+
+    print(beegeek())
+    print(beegeek.__name__)
+    print(beegeek.__doc__)
