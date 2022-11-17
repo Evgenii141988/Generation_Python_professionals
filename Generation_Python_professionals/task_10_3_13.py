854ca7e2c5ae715827e075ea3d4e0fcfb3221065
Evgenii Zalesski <evgenii889111@gmail.com>
2022-11-17 20:27:33 +0300

    task 10.3.13 Дополните приведенный ниже код, чтобы в переменной infinite_love содержался итератор, бесконечно генерирующий единственное значение — строку i love beegeek!.

---
+++ Generation_Python_professionals/task_10_3_13.py
@@ -0,0 +1,8 @@
+
+if __name__ == '__main__':
+    infinite_love = iter(lambda: 'i love beegeek!', 0)
+    print(next(infinite_love))
+    print(next(infinite_love))
+    print(next(infinite_love))
+    print(next(infinite_love))
+    print(next(infinite_love))
