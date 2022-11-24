2c9c793b9d1215f556edfe2ec2a4b035cd0ea692
Evgenii Zalesski <evgenii889111@gmail.com>
2022-11-24 19:52:20 +0300

    task 10.5.23 Функция matrix_by_elem() Вам доступна генераторная функция matrix_by_elem(), которая принимает в качестве аргумента матрицу произвольной размерности и возвращает генератор, порождающий последовательность элементов переданной матрицы.
    Перепишете данную функцию с использованием конструкции yield from, чтобы она выполняла ту же задачу.
    
    Примечание 1. Под матрицей подразумеваются исключительно вложенные списки.

---
+++ Generation_Python_professionals/task_10_5_23.py
@@ -0,0 +1,11 @@
+def matrix_by_elem(matrix):
+    for row in matrix:
+        yield from row
+
+
+if __name__ == '__main__':
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    print(*matrix_by_elem(matrix))
