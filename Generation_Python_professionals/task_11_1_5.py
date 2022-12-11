24d3202666a28cfd82c36fef72ca3008743a33a1
Evgenii Zalesski <evgenii889111@gmail.com>
2022-12-11 20:03:31 +0300

    task 11.1.5 Я в аду? Напишите программу, которая в заданном тексте находит все телефонные номера, соответствующие следующим форматам:
    7-ddd-ddd-dd-dd
    8-ddd-dddd-dddd
    где d — цифра от 00 до 99.
    
    Формат входных данных
    На вход программе подается строка произвольного содержания.
    
    Формат выходных данных
    Программа должна в введенном тексте найти все телефонные номера, соответствующие форматам, указанным в условии задачи, и вывести их в том порядке, в котором они были найдены, каждый на отдельной строке.

--- Generation_Python_professionals/task_11_1_5.py
+++ Generation_Python_professionals/task_11_1_5.py
@@ -2,7 +2,9 @@ import re
 
 if __name__ == '__main__':
     string = input()
+
     shablon = r'8-\d{3}-\d{4}-\d{4}|7-\d{3}-\d{3}-\d{2}-\d{2}'
+
     numbers = re.findall(shablon, string)
     for number in numbers:
         print(number)
