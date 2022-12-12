0d25c96c9bde40a7701f73201f0e5c4858ee3f46
Evgenii Zalesski <evgenii889111@gmail.com>
2022-12-12 20:31:19 +0300

    task 11.3.17 PAN (Permanent Account Number) – это уникальный номер, который присваивается всем налогоплательщикам в Индии. Он имеет следующий формат:
    <letter><letter><letter><letter><letter><digit><digit><digit><digit><letter>
    PAN всегда состоит из 10 символов, в котором letter — заглавная латинская буква, а digit — цифра.
    
    Дополните приведенный ниже код, чтобы переменная regex содержала регулярное выражение, которому соответствуют PAN номера.

--- Generation_Python_professionals/task_11_1_5.py
+++ Generation_Python_professionals/task_11_1_5.py
@@ -1,11 +1,10 @@
 import re
 
 if __name__ == '__main__':
-    string = input()
+    string = 'The PAN (or PAN number) is a ten-character long alpha-numeric unique identifier. Example: AAAPZ1234C first number is ABCD EZZPA1234ZaPMQ0000O, check thusEZZPA1234ZAPMQ0000O, '
 
-    shablon = r'8-\d{3}-\d{4}-\d{4}|7-\d{3}-\d{3}-\d{2}-\d{2}'
+    shablon = r'[A-Z]{5}\d{4}[A-Z]'
 
     numbers = re.findall(shablon, string)
     for number in numbers:
         print(number)
-
