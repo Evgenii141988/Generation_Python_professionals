d213c89dac5551d775d1fbc3fad3630c36885427
Evgenii Zalesski <evgenii889111@gmail.com>
2022-12-14 20:48:33 +0300

    task 11.5.6 Дополните приведенный ниже код, чтобы переменная regex содержала регулярное выражение, которому соответствуют последовательности символов, удовлетворяющие следующим условиям:
    последовательность должна состоять только из bee и geek
    последовательность должна содержать хотя бы один geek
    bee не может находиться рядом с самим собой (не может быть beebee)
    geek может появиться только после того, как до этого было записано bee
    после каждого bee когда-нибудь должен появиться ge

--- Generation_Python_professionals/task_11_4_9.py
+++ Generation_Python_professionals/task_11_4_9.py
@@ -3,9 +3,18 @@ import re
 if __name__ == '__main__':
     string = 'An acle, a Ancle, A antarktida, an Any'
 
-
     shablon = r'\b[Aa]n?\b'
+    regex = r'^[MDE]r\.[A-Za-z]+$|^Mr?s\.[A-Za-z]+$'
+    reg = '((bee)(geek)+)+'
 
     numbers = re.findall(shablon, string)
     for number in numbers:
         print(number)
+
+    result = re.findall(regex, 'Mr.Guev')
+    for elm in result:
+        print(elm)
+
+    new_result = re.findall(reg, 'Correct name is beegeekbeegeek hello beegeek_cyber_school beebeegeekgeekgeekbee')
+    for elm in new_result:
+        print(elm)
