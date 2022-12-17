de280eec9618e4563d59147ab36d8569cafddc8b
Evgenii Zalesski <evgenii889111@gmail.com>
2022-12-17 17:01:29 +0300

    task 11.6.8 Телефонные номера Вам доступен набор телефонных номеров, имеющих следующие форматы:
    <код страны>-<код города>-<номер>
    <код страны> <код города> <номер>
    в котором код страны и код города представлены последовательностями от одной до трех цифр включительно, а номер — последовательностью от четырех до десяти цифр включительно. Между кодом страны, кодом города и номером используется разделитель, которым служит либо символ дефис, либо пробел, причем одновременно оба вида разделителя в одном номере присутствовать не могут.
    
    Напишите программу, которая принимает произвольное количество телефонных номеров и для каждого выводит отдельно его код страны, код города и номер.
    
    Формат входных данных
    На вход программе подается произвольное количество телефонных номеров, удовлетворяющих приведенным выше шаблонам, каждый на отдельной строке.
    
    Формат выходных данных
    Программа должна для каждого введенного телефонного номера вывести отдельно его код страны, код города и номер в следующем формате:
    
    Код страны: <код страны>, Код города: <код города>, Номер: <номер>

---
+++ Generation_Python_professionals/task_11_6_8.py
@@ -0,0 +1,11 @@
+from re import search, match
+import sys
+
+if __name__ == '__main__':
+    # numbers = [string.strip() for string in sys.stdin]
+    numbers = ['1 877 2638277', '91-011-23413627']
+    reg = r'(?P<country>\d{1,3})([\s-])(?P<city>\d{1,3})\2(?P<number>\d{4,10})'
+    for num in numbers:
+        match1 = match(reg, num)
+        if match:
+            print(f'Код страны: {match1.group("country")}, Код города: {match1.group("city")}, Номер: {match1.group("number")}')
