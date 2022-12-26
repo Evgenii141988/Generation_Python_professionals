39ad041d64b0b854918ae75994363e24f9842d89
Evgenii Zalesski <evgenii889111@gmail.com>
2022-12-26 22:59:09 +0300

    task 11.8.13 Повторяющиеся слова 🌶️ Напишите программу, которая заменяет все повторяющиеся рядом стоящие слова на одно слово.
    Формат входных данных
    На вход программе подается строка, содержащая слова.
    
    Формат выходных данных
    Программа должна в введенной строке заменить все повторяющиеся рядом стоящие слова на одно слово и вывести полученный результат.
    
    Примечание 1. Программа должна быть чувствительной к регистру, то есть, например, слова python и Python считаются различными.
    
    Примечание 2. Словом будем считать последовательность символов, соответствующих \w, окруженную символами, соответствующими \W.

---
+++ Generation_Python_professionals/task_11_8_13.py
@@ -0,0 +1,13 @@
+import re
+
+
+def get_string(math_obj) -> str:
+    return math_obj.group(1) + math_obj.group(2)
+
+
+if __name__ == '__main__':
+    string = input()
+    n = 1
+    while n:
+        string, n = re.subn(r'\b(\W*)(\w+)(\W*)\b(\W*)\b\2\b', get_string, string)
+        print(string)
