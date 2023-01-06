09b0e5b1ebcef3281399a9bbcdb40e421cb911eb
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-06 23:19:03 +0300

    task 2.10.2 На вход вашей программе поступают координаты точки x и y - два целых числа, каждое вводится на отдельной строчке.
    Ваша задача обязательно сохранить поступающие на вход значения в переменные x и y соответственно, и затем вывести строку вида Точка(x = {значение}, y = {значение})

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,6 +1,6 @@
 import sys
 if __name__ == '__main__':
-    numbers = map(int, [num.strip() for num in sys.stdin])
-    # print([hex(num)[2:].upper().zfill(2) for num in numbers])
-    print(f'#{"".join([hex(num)[2:].upper().zfill(2) for num in numbers])}')
+    x, y = map(int, [num.strip() for num in sys.stdin])
+    print(f'Точка({x = }, {y = })')
+
 
