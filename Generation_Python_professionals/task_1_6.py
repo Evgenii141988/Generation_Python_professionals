bf23de82eaf6655a4384a57dd5d79b47a8c5adbe
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-07 12:37:56 +0300

    task 2.13.4 Метод append В вашем распоряжении список numbers. Ваша задача добавить по порядку четыре следующих элемента в конец списка: 111, 222, 789 и 201
     В качестве ответа необходимо вывести измененный список numbers

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,8 +1,6 @@
 import sys
+
 if __name__ == '__main__':
-    text = input()
-    print(f"|{text:&^20}|")
-    print(f"|{text:&>20}|")
-    print(f"|{text:&<20}|")
-
-
+    numbers = [-214, 181, -139, 448, -20, -917, 32, 422, -895, 198, 284, 472, -986, -964, -989, 29]
+    numbers.extend([111, 222, 789, 201])
+    print(numbers)
