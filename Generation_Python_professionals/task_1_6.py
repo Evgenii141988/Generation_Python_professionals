7a46a6587abce1f4a97b6cdb647bb9c52c08cb65
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-07 12:43:50 +0300

    task 2.13.5 Метод insert В вашем распоряжении список numbers. Ваша задача выполнить четыре следующих действия в том же порядке, как они указаны в списке:
    добавить значение 111  на 5-й индекс;
    добавить значение 222  на 8-й индекс;
    добавить значение 789  на 0-й индекс;
    добавить значение 201  на 11-й индекс;
     В качестве ответа необходимо вывести измененный список numbers

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -2,5 +2,7 @@ import sys
 
 if __name__ == '__main__':
     numbers = [-214, 181, -139, 448, -20, -917, 32, 422, -895, 198, 284, 472, -986, -964, -989, 29]
-    numbers.extend([111, 222, 789, 201])
+    inserts = ((111, 5), (222, 8), (789, 0), (201, 11))
+    for num, i in inserts:
+        numbers.insert(i, num)
     print(numbers)
