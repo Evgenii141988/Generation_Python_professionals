e01a931497b2b1ba0bfcd26bfe60e42c16abbcd3
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-07 12:58:33 +0300

    task 2.13.7 Метод remove В вашем распоряжении список numbers. Ваша задача удалить из этого списка числа 3, 5, 7 и 9.
     В качестве ответа необходимо вывести измененный список numbers

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,8 +1,10 @@
 import sys
 
-if __name__ == '__main__':
-    numbers = [-214, 181, -139, 448, -20, -917, 32, 422, -895, 198, 284, 472, -986, -964, -989, 29]
-    indexs = (-1, 0, 12, 7)
-    sum_elements = sum(numbers.pop(i) for i in indexs)
-    print(numbers)
-    print(sum_elements)
+    if __name__ == '__main__':
+        numbers = [-214, 777, 181, 9, 32, -139, 43, 89, 77, 448, -20, -917, 54, 5, 432, 43, 32, 422, -895, 7, 198, 284, 472,
+                   3, -986, -964, -989, 29]
+        elements = (3, 5, 7, 9)
+        for elm in elements:
+            numbers.remove(elm)
+        print(numbers)
+        numbers.sort(reverse=True)
