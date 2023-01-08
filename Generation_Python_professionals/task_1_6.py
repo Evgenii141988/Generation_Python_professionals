5cf01eb4fec5e76dc7593d8f9aa4a37f2380baca
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-08 15:39:03 +0300

    Дана строка из строчных букв английского алфавита. Необходимо найти количество пар букв 'a' и 'b', таких, что буква 'a' находится раньше 'b'.

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,10 +1,11 @@
-import sys
+import sys, re
 
-    if __name__ == '__main__':
-        numbers = [-214, 777, 181, 9, 32, -139, 43, 89, 77, 448, -20, -917, 54, 5, 432, 43, 32, 422, -895, 7, 198, 284, 472,
-                   3, -986, -964, -989, 29]
-        elements = (3, 5, 7, 9)
-        for elm in elements:
-            numbers.remove(elm)
-        print(numbers)
-        numbers.sort(reverse=True)
+if __name__ == '__main__':
+    string = input()
+    count = 0
+    for i, lit in enumerate(string):
+        if lit == 'a':
+            for s in string[i + 1:]:
+                if s == 'b':
+                    count += 1
+    print(count)
