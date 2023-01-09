de7f56784139882ba1595e6c4aec20f5b7016864
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-09 22:00:34 +0300

    Set(func), которая будет обновлять некую функцию преобразования строки. Compute(string) которая будет рассчитывать и возвращать значение func(string).

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,31 +1,19 @@
-def cmp_prev(val, numbers=[]):
-    if len(numbers) < 1:
-        numbers.append(val)
-        return False
-    flag = val > sum(numbers[-2:])
-    numbers.append(val)
-    return flag
+new_func = ''
+
+
+def Set(func):
+    global new_func
+    new_func = func
+
+
+def Compute(string):
+    global new_func
+    return new_func(string)
 
 
 if __name__ == '__main__':
-    assert cmp_prev(0) == False
-    assert cmp_prev(1) == True
-    assert cmp_prev(68) == True
-    assert cmp_prev(57) == False
-    assert cmp_prev(346) == True
-    assert cmp_prev(4) == False
-    assert cmp_prev(5) == False
-    assert cmp_prev(10) == True
-    assert cmp_prev(15) == False
-    assert cmp_prev(-7) == False
-    assert cmp_prev(8) == False
-    assert cmp_prev(2) == True
-    assert cmp_prev(-6) == False
-    assert cmp_prev(-7) == False
-    assert cmp_prev(-6) == True
-    assert cmp_prev(0) == True
-    assert cmp_prev(-5) == True
-    assert cmp_prev(65) == True
-    assert cmp_prev(456) == True
-    assert cmp_prev(-678) == False
-    assert cmp_prev(77) == True
+    Set(len)
+    print(Compute('Hello'))  # 5
+    print(Compute('129'))  # 3
+    Set(max)
+    print(Compute('129'))  # 9
