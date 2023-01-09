4e215aecefce41b3e21f9259ccc2764a68f24fa2
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-09 20:02:15 +0300

    Реализуйте функцию calls_num(), которая будет возвращать число предыдущих вызовов данной функции.

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,13 +1,14 @@
-def almost_double_factorial(n: int) -> int:
-    mul = 1
-    for i in range(1, n + 1):
-        if i % 2 != 0:
-            mul *= i
-    return mul
+counter = -1
+
+
+def calls_num():
+    global counter
+    counter += 1
+    return counter
 
 
 if __name__ == '__main__':
-    print(almost_double_factorial(10))
-    print(almost_double_factorial(50))
-    print(almost_double_factorial(100))
-    print(almost_double_factorial(0))
+    print(calls_num())
+    print(calls_num())
+    print(calls_num())
+    print(calls_num())
