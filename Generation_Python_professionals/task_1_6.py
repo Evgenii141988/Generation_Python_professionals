756d6298eb06daca7b012a0709b68ec0cd03969e
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-11 19:21:08 +0300

    Если расположить рядом два магнита, они начинают взаимодействовать друг с другом. При этом одинаковые магнитные полюса (север/север или юг/юг) отталкиваются друг от друга, в то время как разные магнитные полюса (север/юг) притягиваются друг к другу.
    Ваша программа получает два значения в разных строках - полярности магнитов, которые могут иметь значения либо N ( север) либо S (юг)
    
    Ваша задача сохранить в переменную experiment строку Притягиваются, если магниты имеют разную полярность, в противном случае сохраните строку Отталкиваются
    
    В качестве ответа необходимо вывести переменную experiment

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
-    string = input()
-    sentence = 'Вопросительное' if string[-1] == '?' else 'Обычное'
-    print(sentence)
+    one = input()
+    two = input()
+    experiment = 'Притягиваются' if one in ('N', 'S') and two in ('N', 'S') and one != two else 'Отталкиваются'
+    print(experiment)
