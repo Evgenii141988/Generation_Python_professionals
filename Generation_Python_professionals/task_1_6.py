9db6685b146c5240addbd9e2209563756ce39794
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-11 19:14:57 +0300

    На вход вашей программе поступает два неравных друг друг целых числа в отдельных строках
    Ваша задача сохранить наименьшее из этих чисел в переменную  minimum, а наибольшее - в maximum
    
    Использовать нужно, конечно же, тернарный оператор
    
    В качестве ответа выведите через пробел сперва minimum , а потом maximum

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    if (money := int(input())) <= 10000:
-        print(f'Сумма {money} не превышает лимит, проходите')
-    else:
-        print(f'Ого! {money}! Куда вам столько? Мы заберем {money - 10000}')
+    a = int(input())
+    b = int(input())
+    minimum, maximum = (a, b) if a < b else (b, a)
+    print(minimum, maximum, sep=' ')
