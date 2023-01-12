80a4451b141ac309d5beb4004bbb06441473a9a8
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-12 22:06:24 +0300

    Давайте попробуем потренироваться в операторе match-case
    Чуть ранее студенты технических специальностей университета учились 5 лет (специалитет) и затем им вручался аттестат. Ваша программа программа будет получать на вход целое число - номер курса, и в зависимости от номера выводить следующий текст
    
    если ввели 1, выведите сообщение Совсем еще зеленый студентик
    если ввели 2, выведите сообщение Джун-студент
    если ввели 3, выведите сообщение Мидл-студент
    если ввели 4, выведите сообщение Сеньер-студент
    если ввели 5, выведите сообщение Босс качалки
    при вводе остальных значений, выведите текст Неизвестный курс
    Используйте при решении оператор match-case

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,5 +1,15 @@
 if __name__ == '__main__':
-    one = input()
-    two = input()
-    experiment = 'Притягиваются' if one in ('N', 'S') and two in ('N', 'S') and one != two else 'Отталкиваются'
-    print(experiment)
+    n = int(input())
+    match n:
+        case 1:
+            print('Совсем еще зеленый студентик')
+        case 2:
+            print('Джун-студент')
+        case 3:
+            print('Мидл-студент')
+        case 4:
+            print('Сеньер-студент')
+        case 5:
+            print('Босс качалки')
+        case _:
+            print('Неизвестный курс')
