394972a7cf981e5104def2d6f07cbe906077ec63
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-12 22:15:10 +0300

    Тип знака зодиака Программа ваша получает на вход строку - название знака зодиака и затем сообщает к какому типу относится введенный знак. Логика определения следующая:
    если введут любое из значений Овен, Лев, Стрелец, то программа выводит текст Огненный
    если введут любое из значений Телец, Дева, Козерог, то программа выводит текст Земной
    если введут любое из значений Близнецы, Весы, Водолей, то программа выводит текст Воздушный
    если введут любое из значений Рак, Скорпион, Рыбы, то программа выводит текст Водный

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
-    n = int(input())
-    match n:
-        case 1 | 3 | 5 | 7 | 8 | 10 | 12:
-            print(31)
-        case 4 | 6 | 9 | 11:
-            print(30)
-        case 2:
-            print(28)
+    sign = input()
+    match sign:
+        case 'Овен' | 'Лев' | 'Стрелец':
+            print('Огненный')
+        case 'Телец' | 'Дева' | 'Козерог':
+            print('Земной')
+        case 'Близнецы' | 'Весы' | 'Водолей':
+            print('Воздушный')
         case _:
-            print('Неизвестный месяц')
+            print('Водный')
