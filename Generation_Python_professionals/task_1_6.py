14f149e7132c91a929af2cab759c5ded27d5ac6b
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-12 22:10:47 +0300

    Сколько дней в месяце? Часто можно услышать такой вопрос. Давайте это запрограммируем.
    Программа получает на вход номер месяца - натуральное число N (1 ≤ N ≤ 12) и в зависимости от его значения выводит количество дней в месяце. Будем считать, что год невисокосный. При решении конечно же используйте оператор match-case

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,15 +1,11 @@
 if __name__ == '__main__':
     n = int(input())
     match n:
-        case 1:
-            print('Совсем еще зеленый студентик')
+        case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+            print(31)
+        case 4 | 6 | 9 | 11:
+            print(30)
         case 2:
-            print('Джун-студент')
-        case 3:
-            print('Мидл-студент')
-        case 4:
-            print('Сеньер-студент')
-        case 5:
-            print('Босс качалки')
+            print(28)
         case _:
-            print('Неизвестный курс')
+            print('Неизвестный месяц')
