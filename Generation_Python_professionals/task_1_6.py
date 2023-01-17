7a0e62d88681cd97598156eca9584c1ff573be45
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-17 18:10:23 +0300

    Перед вами кортеж words_tuple
    При помощи цикла for обойдите слова, хранящиеся в кортеже words_tuple, и для каждого элемента выведите строку вида
    
    Длина слова {word} = {len_word}
    Например, для кортежа words_tuple=('hi', 'world') ответ был бы таким:
    
    Длина слова hi = 2
    Длина слова world = 5

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,9 +1,6 @@
 if __name__ == '__main__':
-    words = input().split()
-    new_words = []
-    words_lower = []
-    for word in words:
-        if word.lower() not in words_lower:
-            new_words.append(word)
-            words_lower.append(word.lower())
-    print(' '.join(new_words))
+    words_tuple = ('quaint', 'leftovers', 'thesis', 'density', 'retired', 'weak', 'tolerate',
+                   'sensitivity', 'primary', 'definition', 'determine', 'bring', 'monstrous',
+                   'hurl', 'timetable', 'month', 'advocate', 'provoke', 'stress', 'omission')
+    for word in words_tuple:
+        print(f'Длина слова {word} = {len(word)}')
