5979847d8582572406d6f4096f867c96e850a8a0
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-20 19:08:09 +0300

    Перед вами располагается словарь address
    Давайте проверим наличие следующих ключей:
    
    zip_code
    longitude
    post_code
    street_name
    number_house
    Программа должна проверить наличие ключей и вывести True, если ключ имеется,  или False, если ключ отсутствует.
    
    Проверки ключей должны выполняться в отдельной строчке в порядке перечисления ключей в задании

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,5 +1,34 @@
 from string import ascii_lowercase
 
 if __name__ == '__main__':
-    alphabet = {lit: i for i, lit in enumerate(ascii_lowercase, 1)}
-    print(alphabet)
+    address = {
+        "id": 7973,
+        "uid": "42f2ce1d-90ab-4345-9595-0d9f42e6c085",
+        "city": "East Monteland",
+        "street_name": "Gusikowski Land",
+        "street_address": "3230 Daugherty Centers",
+        "secondary_address": "Apt. 562",
+        "building_number": "58671",
+        "mail_box": "PO Box 5313",
+        "community": "Paradise Square",
+        "zip_code": "58611",
+        "zip": "01667",
+        "postcode": "00563",
+        "time_zone": "America/New_York",
+        "street_suffix": "Burg",
+        "city_suffix": "mouth",
+        "city_prefix": "West",
+        "state": "Wisconsin",
+        "state_abbr": "NY",
+        "country": "Mali",
+        "country_code": "MH",
+        "latitude": -56.97457993706476,
+        "longitude": -104.29509072140858,
+        "full_address": "Apt. 982 4820 Leena Rest, Lake Giannaville, MN 09265-3715"
+    }
+    check_keys = ['zip_code',
+                  'longitude',
+                  'post_code',
+                  'street_name',
+                  'number_house']
+    print(*[key in address for key in check_keys], sep='\n')
