02e576afc4874e80a572198002f73c34386b5915
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-21 16:01:45 +0300

    Напишите код для преобразования списка из целых чисел произвольной длины в словарь, вложенность которого зависит от длины списка.
    Например, если перед вами был бы такой список
    
    [100, 55, 77, 55, 89]
    то он должен превратится в такой словарь
    
    {100: {55: {77: {55: 89}}}}
    На вход программе поступают числа для списка в одну строку, гарантируется, что в списке будет как минимум два элемента.
    
    Ваша задача вывести полученный словарь

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,20 +1,10 @@
-from string import ascii_lowercase
+def get_dict(nums: list) -> dict:
+    if len(nums) == 2:
+        return {nums[0]: nums[1]}
+    else:
+        return {nums[0]: get_dict(nums[1:])}
+
 
 if __name__ == '__main__':
-    user = {
-        "id": 4170,
-        "uid": "5e941db5-9e0f-4f94-9fc5-734110c6be14",
-        "password": "SyUpfo1ljm",
-        "first_name": "Teresa",
-        "last_name": "Wehner",
-        "username": "teresa.wehner",
-        "email": "teresa.wehner@email.com",
-        "gender": "Non-binary",
-        "phone_number": "+674 424.561.2776",
-        "social_insurance_number": "637316241",
-        "date_of_birth": "1993-08-17"
-    }
-    user['secret'] = user.pop('password')
-    user['surname'] = user.pop('last_name')
-
-    print(user)
+    numbers = list(map(int, input().split()))
+    print(get_dict(numbers))
