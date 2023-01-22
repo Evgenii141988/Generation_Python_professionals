012fdee8a3713db1d08f85110c18c9a293d561d7
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-22 13:23:53 +0300

    Во сколько обойдется данная покупка? Хотим купить все товары в указанном количестве

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,10 +1,20 @@
 
 
 if __name__ == '__main__':
-    workers = {
-        'employer1': {'name': 'Jhon', 'salary': 7500},
-        'employer2': {'name': 'Emma', 'salary': 8000},
-        'employer3': {'name': 'Brad', 'salary': 500}
+    supermarket = {
+        "milk": {"quantity": 20, "price": 1.19},
+        "biscuits": {"quantity": 32, "price": 1.45},
+        "butter": {"quantity": 20, "price": 2.29},
+        "cheese": {"quantity": 15, "price": 1.90},
+        "bread": {"quantity": 15, "price": 2.59},
+        "cookies": {"quantity": 20, "price": 4.99},
+        "yogurt": {"quantity": 18, "price": 3.65},
+        "apples": {"quantity": 35, "price": 3.15},
+        "oranges": {"quantity": 40, "price": 0.99},
+        "bananas": {"quantity": 23, "price": 1.29}
     }
-    workers['employer3']['salary'] = 8500
-    print(workers)
+    count = 0
+    for value in supermarket.values():
+        count += value['quantity'] * value['price']
+
+    print(count)
