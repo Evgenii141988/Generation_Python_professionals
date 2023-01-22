a010e8343a468c8a7114d8599ec7d65154c76d6b
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-22 13:41:46 +0300

    Декартово произведение Декартово произведение двух множеств — множество, элементами которого являются все возможные упорядоченные пары элементов исходных множеств. Под множествами в этой задачи мы будем считать списки, один из них будет содержать цвета маек, а другой размеры одежды.
    Если объяснять более простым языком, то  декартово произведение это когда вы каждый элемент из одного множества группируете с каждый элементом другого множества. Например, если бы у вас имелись такие списки
    
    colors = ['red', 'green']
    sizes = ['S', 'M', 'L']
    то их декартово произведение выглядело бы так
    
    [('red', 'S'), ('red', 'M'), ('red', 'L'), ('green', 'S'), ('green', 'M'), ('green', 'L')]
    Здесь мы берем каждый цвет и сочетаем его с каждым размером. Общее количество элементов в данном декартовом множестве рассчитывается по формуле
    
    len(colors) * len(sizes)
    Ваша задача создать список кортежей на основании переменных colors и sizes.
    
    Обратите внимание на порядок элементов в ответе, сперва мы берем первый цвет и для него перебираем все возможные размеры. Порядок формирования в этом задании важен
    
    В качестве ответа выведите на экран полученный список кортежей

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,28 +1,9 @@
 import pprint
 
 if __name__ == '__main__':
-    user = {
-        "id": 4170,
-        "uid": "5e941db5-9e0f-4f94-9fc5-734110c6be14",
-        "password": "SyUpfo1ljm",
-        "first_name": "Teresa",
-        "last_name": "Wehner",
-        "username": "teresa.wehner",
-        "email": "teresa.wehner@email.com",
-        "gender": "Non-binary",
-        "phone_number": "+674 424.561.2776",
-        "social_insurance_number": "637316241",
-        "date_of_birth": "1993-08-17",
-        "employment": {
-            "title": "Central Hospitality Liaison",
-            "key_skill": "Organisation"
-        },
-        "subscription": {
-            "plan": "Essential",
-            "status": "Idle",
-            "payment_method": "Debit card",
-            "term": "Annual"
-        }
-    }
-    new_dict = {key: user[key] for key in input().split()}
-    print(new_dict)
+    colors = ['White', 'Blue', 'Yellow', 'Purple', 'Black', 'Green']
+    sizes = ['S', 'M', 'L', 'XL', 'XLL']
+    result = [(color, size) for color in colors for size in sizes]
+    print(result)
+
+
