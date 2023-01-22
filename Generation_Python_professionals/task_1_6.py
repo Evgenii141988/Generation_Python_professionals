702dcb12668d4ab68715eef29874be7b71c5e9e5
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-22 13:21:13 +0300

    Перед вами словарь workers
    Ваша задача поднять зарплату Бреду до 8500 и затем вывести измененный словарь workers

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,10 +1,10 @@
-def get_dict(nums: list) -> dict:
-    if len(nums) == 2:
-        return {nums[0]: nums[1]}
-    else:
-        return {nums[0]: get_dict(nums[1:])}
 
 
 if __name__ == '__main__':
-    numbers = list(map(int, input().split()))
-    print(get_dict(numbers))
+    workers = {
+        'employer1': {'name': 'Jhon', 'salary': 7500},
+        'employer2': {'name': 'Emma', 'salary': 8000},
+        'employer3': {'name': 'Brad', 'salary': 500}
+    }
+    workers['employer3']['salary'] = 8500
+    print(workers)
