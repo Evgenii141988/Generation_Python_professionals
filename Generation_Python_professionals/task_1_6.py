0206d8c149740d3ba26b646d8c8c759b1fc83d6c
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-24 20:52:23 +0300

    Перед вами располагается список words, состоящий из 55 слов, которые могут повторяться
    Ваша задача вывести на экран количество уникальных слов, длина которых больше 6.
    
    Под уникальностью здесь подразумевается то, что в случае возникновения дублирующих слов, в подсчете вы не должны учитывать дубли.

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,5 +1,12 @@
 import pprint
 
 if __name__ == '__main__':
-    my_set = set(input().strip('{}').split(', '))
-    print(len(my_set))
+    words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+             'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
+             'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+             'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+             'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
+             'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government',
+             'control', 'value', 'few', 'generation', 'service', 'national',
+             'tradition', 'government', 'mention', 'proposal']
+    print(len(set(word for word in words if len(word) > 6)))
