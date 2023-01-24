fc31a1f9cb564ba99e42c9d9e813df1e9d2ecbc4
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-24 21:11:20 +0300

    Вашей программе будут поступать на вход N списков, содержащих целые числа
    Для каждого введенного списка определите, сколько в нем встречается различных чисел.
    
    Входные данные
    
    Сперва поступает натуральное число N - количество списков
    
    В следующих N строк вводятся значения каждого списка, разделенные через пробел
    
    Выходные данные
    
    Вывести на отдельной строке количество различных чисел каждого введенного списка в том же порядке, в котором вводились списки

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,12 +1,6 @@
 import pprint
 
 if __name__ == '__main__':
-    words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
-             'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
-             'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
-             'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
-             'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
-             'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government',
-             'control', 'value', 'few', 'generation', 'service', 'national',
-             'tradition', 'government', 'mention', 'proposal']
-    print(len(set(word for word in words if len(word) > 6)))
+    n = int(input())
+    len_list = [len(set(list(map(int, input().split())))) for _ in range(n)]
+    print(*len_list, sep='\n')
