11e08300d6b38df0a6c2967648d5143fa23c913c
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-25 21:29:43 +0300

    Перед вами располагается множество  my_set
    Ваша задача удалить из него 3 строковых элемента:
    
    government
    national
    tease
    Выводить ничего не нужно, только удалить элементы выше

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,7 +1,15 @@
 import pprint
 
 if __name__ == '__main__':
-    my_set = {'government', 'control', 'winter', 'few', 'generation',
-              'service', 'national', 'tradition', 'government'}
-        my_set.update(['concert', 'brown', 'jacket', 'value'])
-    print(my_set)
+    my_set = {
+        'mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+        'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
+        'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+        'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+        'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention',
+        'tradition', 'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal',
+        'government', 'control', 'value', 'few', 'generation', 'service', 'national',
+        'tradition', 'government', 'mention', 'proposal'
+    }
+    for elm in ['government', 'national', 'tease']:
+        my_set.remove(elm)
