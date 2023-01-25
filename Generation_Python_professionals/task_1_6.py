31bbc0edad38f5cc93e717fd57d858934ab00e80
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-25 22:28:13 +0300

    Вашей программе будут поступать на вход N списков, содержащих целые числа
    Ваша задача определить сколько всего встречалось различных чисел во всех этих списках
    
    Входные данные
    
    Сперва поступает натуральное число N - количество списков
    
    В следующих N строк вводятся значения каждого списка, разделенные через пробел
    
    Выходные данные
    
    Вывести одно число - количество различных чисел во всех этих списках

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,15 +1,8 @@
 import pprint
 
 if __name__ == '__main__':
-    my_set = {
-        'mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
-        'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
-        'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
-        'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
-        'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention',
-        'tradition', 'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal',
-        'government', 'control', 'value', 'few', 'generation', 'service', 'national',
-        'tradition', 'government', 'mention', 'proposal'
-    }
-    for elm in ['government', 'national', 'tease']:
-        my_set.remove(elm)
+    new_set = set()
+    for _ in range(int(input())):
+        new_set.update(map(int, input().split()))
+    print(len(new_set))
+
