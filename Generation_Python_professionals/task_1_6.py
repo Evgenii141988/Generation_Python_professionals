f1d3abe3ce7a083d83f4a5547aec796eb2a052c5
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-27 21:58:52 +0300

    Перед вами имеется список words
    Ваша задача на основании создать список кортежей words_with_position, каждый элемент-кортеж должен содержать два значения: само слово и его порядковый номер в списке words
    
    Порядковый номер слов необходимо считать с единицы. Вот к примеру, если бы список words был таким:
    
    words = ['variation', 'random', 'electronics', 'competence', 'collapse']
    то на выходите вы должны были получить такой ответ
    
    words_with_position = [('variation', 1),
                           ('random', 2),
                           ('electronics', 3),
                           ('competence', 4),
                           ('collapse', 5)]
    В качестве ответа необходимо вывести words_with_position

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,8 +1,8 @@
 import pprint
 
 if __name__ == '__main__':
-    my_set = set()
-    words = input().lower().split(',')
-    for word in words:
-        print('ДА' if word in my_set else 'НЕТ')
-        my_set.add(word)
+    words = ['feel', 'graduate', 'movie', 'fashionable', 'bacon',
+             'drop', 'produce', 'acquisition', 'cheap', 'strength',
+             'master', 'perception', 'noise', 'strange', 'am']
+    words_with_position = [(elm, i) for i, elm in enumerate(words, 1)]
+    print(words_with_position)
