963f9eed90e3f6bef15a0257418e59dd8be00ddf
Evgenii Zalesski <evgenii889111@gmail.com>
2023-01-29 12:49:34 +0300

    Напишите функцию exponentiation, которая принимает на вход целое число и выводит на экран через пробел квадрат и куб этого числа.
    Вам необходимо написать только определение функции exponentiation

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,14 +1,6 @@
-import pprint
+def exponentiation(num: int) -> str:
+    print(f'{num ** 2} {num ** 3}')
+
 
 if __name__ == '__main__':
-    number = input()
-    count = 0
-    for i, num in enumerate(number[::-1], 1):
-        num = int(num)
-        if i % 2 == 0:
-            num *= 2
-            if num > 9:
-                num -= 9
-        count += num
-    print(count % 10 == 0)
-
+    exponentiation(int(input()))
