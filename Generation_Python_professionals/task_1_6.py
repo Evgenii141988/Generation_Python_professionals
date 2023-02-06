cce2bea0e29a157c5470b1a0ccbf04aa960e1a8a
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-06 20:56:06 +0300

    Заменяем символы Напишите функцию replace, которая принимает три параметра:
    обязательный строковый параметр text - текст, в котором необходимо выполнить замены;
    обязательный строковый параметр old - строка поиска для замены;
    необязательный строковый параметр new - значение замены для найденного значения old. По умолчанию равен пустой строке.
    Функция replace должна возвращать новую строку, в которой все символы old были заменены на new. При замены регистр букв должен учитываться
    
    replace('Нет', 'т') => 'Не'
    replace('Bella Ciao', 'a') => 'Bell Cio'
    replace('nobody; i myself farewell analysis', 'l', 'z') => 'nobody; i mysezf farewezz anazysis'
    replace('commend me to my kind lord meaning', 'M', 'w') => 'commend me to my kind lord meaning'
    Ваша задача дописать только тело функции replace

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,23 +1,12 @@
 from collections import Counter
 
 
-def get_word_indices(strings: list[str]) -> dict[str]:
-    result = {}
-    for i, string in enumerate(strings):
-        for word in (elm.lower() for elm in string.split()):
-            result[word] = result.get(word, []) + [i]
-    return result
+def replace(text: str, old: str, new: str = "") -> str:
+    return text.replace(old, new)
 
 
 if __name__ == '__main__':
-    print(get_word_indices(['This is a string',
-                            'test String',
-                            'test',
-                            'string']))
-    print(get_word_indices(['Look at my horse',
-                            'my horse',
-                            'is amazing']))
-    print(get_word_indices([]))
-    print(get_word_indices(['Avada Kedavra',
-                            'avada kedavra',
-                            'AVADA KEDAVRA']))
+    print(replace('Нет', 'т'))
+    print(replace('Bella Ciao', 'a'))
+    print(replace('nobody; i myself farewell analysis', 'l', 'z'))
+    print(replace('commend me to my kind lord meaning', 'M', 'w'))
