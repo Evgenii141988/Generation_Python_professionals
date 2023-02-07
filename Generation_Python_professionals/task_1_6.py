f4341f58c4a2f5b69055f45c6ac97d421c3e9d31
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-07 19:12:45 +0300

    Напишите функцию check_sum, которая принимает произвольное количество аргументов типа int.
    Данная функция должна выводить not enough, если сумма всех элементов меньше 50, в противном случае выводить verification passed
    
    Вам необходимо написать только определение функции check_sum

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,22 +1,10 @@
 from collections import Counter
 
 
-def create_matrix(size: int = 3, up_fill: int = 0, down_fill: int = 0) -> list[list[int]]:
-    result = []
-    for i in range(size):
-        row = []
-        for j in range(size):
-            if i == j:
-                row.append(i + 1)
-            elif j > i:
-                row.append(up_fill)
-            else:
-                row.append(down_fill)
-        result.append(row)
-    return result
+def check_sum(*args: [int]) -> str:
+    return 'not enough' if sum(args) < 50 else 'verification passed'
 
 
 if __name__ == '__main__':
-    print(create_matrix())
-    print(create_matrix(4))
-
+    print(check_sum(8, 11))
+    print(check_sum(10, 10, 10, 10, 9))
