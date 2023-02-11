c83f9b8478fc5893e50efe71d5cd03b15ab56121
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-11 19:33:58 +0300

    Определите функцию print_from, которая принимает одно натуральное число n и распечатывает на экране убывающую последовательность целых чисел от n до 1 включительно. Каждое число необходимо выводить на отдельной строке.

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,8 +1,9 @@
+def print_from(n: int) -> None:
+    if n > 0:
+        print(n)
+        print_from(n - 1)
+
+
 if __name__ == '__main__':
-    n, m = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(n)]
-    for i in range(n - 2, -1, -1):
-        for j in range(m - 2, -1, -1):
-            matrix[i][j] = matrix[i][j + 1] + matrix[i + 1][j]
-    for row in matrix:
-        print(*row, sep=' ')
+    print_from(4)
+    print_from(7)
