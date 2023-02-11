8fda58432e1085f35aae3498bc7b5bbef6c23968
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-11 19:46:58 +0300

    Числа Трибоначчи Описать рекурсивную функцию tribonacci, которая принимает на вход целое число n - порядковый номер чисел Трибоначчи. Функция по параметру n должна вычислить и вернуть значение, стоящее на n-м месте в ряде чисел Трибоначчи

--- Generation_Python_professionals/task_1_6.py
+++ Generation_Python_professionals/task_1_6.py
@@ -1,11 +1,11 @@
-def double_fact(n: int) -> int:
-    if n == 1:
-        return 1
+def tribonacci(n: int) -> int:
+    if n <= 1:
+        return 0
     elif n == 2:
-        return 2
+        return 1
     else:
-        return double_fact(n - 2) * n
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
 
 
 if __name__ == '__main__':
-    print(double_fact(7))
+    print(tribonacci(7))
