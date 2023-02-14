d0e1d3a184cd123b3dc684382f41e7a66a4ed2e8
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-14 18:15:04 +0300

    Ваша задача создать функцию-замыкание create_accumulator, она должна накапливать(суммировать) в себе все значения, которые ей будут переданы, при создании сумма должна быть равна нулю. При каждом вызове должна возвращаться накопленная сумма, которая хранится в замыкании.
    Обратите внимание, что объекты из примера summator_1 и summator_2 хранят и накапливают свои собственные суммы.
    
    Необходимо только определить функцию-замыкание create_accumulator, остальное мы сделаем за вас

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,37 +1,20 @@
-def calculate(x: float, y: float, operation: str = 'a') -> None:
-    def addition(a: float, b: float) -> None:
-        print(a + b)
+def create_accumulator():
+    count = 0
 
-    def subtraction(a: float, b: float) -> None:
-        print(a - b)
+    def inner(n: int):
+        nonlocal count
+        count += n
+        return count
 
-    def division(a: float, b: float) -> None:
-        if b == 0:
-            print('На ноль делить нельзя!')
-            return
-        print(a / b)
-
-    def multiplication(a: float, b: float) -> None:
-        print(a * b)
-
-    match operation:
-        case 'a':
-            addition(x, y)
-        case 's':
-            subtraction(x, y)
-        case 'd':
-            division(x, y)
-        case 'm':
-            multiplication(x, y)
-        case _:
-            print('Ошибка. Данной операции не существует')
+    return inner
 
 
 if __name__ == '__main__':
-    calculate(2, 5)  # Печатает 7.0
-    calculate(2.2, 15, 'a')  # Печатает 17.2
-    calculate(22, 15, 's')  # Печатает 7.0
-    calculate(2, 3.2, 'm')  # Печатает 6.4
-    calculate(10, 0.4, 'd')  # Печатает 25.0
-    calculate(10, 0.4, 'ff')
-    calculate(10, 0, 'd')
+    summator_1 = create_accumulator()
+    print(summator_1(1))  # печатает 1
+    print(summator_1(5))  # печатает 6
+    print(summator_1(2))  # печатает 8
+
+    summator_2 = create_accumulator()
+    print(summator_2(3))  # печатает 3
+    print(summator_2(4))  # печатает 7
