f845eb67b3fc11229dbb1c27ab257a919ba76651
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-15 20:17:08 +0300

    Напишите декоратор text_decor, который оборачивает вызов декорированной функции фразами «Hello» и «Goodbye!»: фраза «Hello» печатается до вызова, фраза «Goodbye!» - после

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,21 +1,22 @@
-def create_dict():
-    elems = {}
-    key = 0
-
-    def inner(value):
-        nonlocal key
-        key += 1
-        elems[key] = value
-        return elems
+def text_decor(func):
+    def inner(*args, **kwargs):
+        print('Hello')
+        func(*args, **kwargs)
+        print('Goodbye!')
 
     return inner
 
 
-if __name__ == '__main__':
-    f_1 = create_dict()
-    print(f_1('hello'))  # f_1 возвращает {1: 'hello'}
-    print(f_1(100))  # f_1 возвращает {1: 'hello', 2: 100}
-    print(f_1([1, 2, 3]))  # f_1 возвращает {1: 'hello', 2: 100, 3: [1, 2, 3]}
+@text_decor
+def simple_func():
+    print('I just simple python func')
 
-    f_2 = create_dict()  # создаем новое замыкание в f_2
-    print(f_2('PoweR'))  # f_2 возвращает {1: 'PoweR'}
+
+@text_decor
+def multiply(num1, num2):
+    print(num1 * num2)
+
+
+if __name__ == '__main__':
+    simple_func()
+    multiply(3, 5)
