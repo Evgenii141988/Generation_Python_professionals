10d2a150cc9a30a136734be1cb3a7a97574f6bff
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-17 19:08:31 +0300

    Напишите декоратор add_args, который добавляет к переданным аргументам еще два значения: строку begin в начало аргументов, строку end в конец. Также декоратор должен сохранить первоначальное имя декорируемой функцию и ее строку документации

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,6 +1,41 @@
-from string import ascii_lowercase, ascii_uppercase, punctuation
+from functools import wraps
+
+
+# Напишите определение декоратора add_args
+def add_args(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        args = ('begin',) + args + ('end',)
+        return func(*args, **kwargs)
+
+    return inner
+
+
+# Код ниже не удаляйте, он нужен для проверки   
+@add_args
+def concatenate(*args):
+    """
+    Возвращает конкатенацию переданных строк
+    """
+    return ', '.join(args)
+
+
+@add_args
+def find_max_word(*args):
+    """
+    Возвращает слово максимальной длины
+    """
+    return max(args, key=len)
+
 
 if __name__ == '__main__':
-    print(punctuation)
-    print(ascii_uppercase)
-    print(ascii_lowercase)
+    print(concatenate('hello', 'world', 'my', 'name is', 'Artem'))
+    assert concatenate('hello', 'world', 'my', 'name is', 'Artem') == 'begin, hello, world, my, name is, Artem, end'
+    assert concatenate('my', 'name is', 'Artem') == 'begin, my, name is, Artem, end'
+    assert concatenate.__name__ == 'concatenate'
+    assert concatenate.__doc__.strip() == """Возвращает конкатенацию переданных строк"""
+    assert find_max_word('my') == 'begin'
+    assert find_max_word('my', 'how') == 'begin'
+    assert find_max_word('my', 'how', 'maximum') == 'maximum'
+    assert find_max_word.__name__ == 'find_max_word'
+    assert find_max_word.__doc__.strip() == """Возвращает слово максимальной длины"""
