a97813164d0010ae409fc854dc80baf8b63dd9ed
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-19 12:58:45 +0300

    Напишите функцию file_n_lines, которая печатает первые n-строка файла. Функция file_n_lines принимает на вход название файла и количество строк для прочтения.
    Не забывайте избавляться от символа переноса строки

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,40 +1,8 @@
-from functools import wraps
-
-
-# Напишите определение декоратора memoize
-def memoize(func):
-    memo = {}
-
-    @wraps(func)
-    def inner(*args, **kwargs):
-        n, = args
-        if n not in memo:
-            memo[n] = func(*args, **kwargs)
-        return memo[n]
-
-    return inner
-
-
-# Код ниже не удаляйте, он нужен для проверки
-
-
-@memoize
-def fibonacci(n):
-    """
-    Возвращает n-ое число Фибоначчи
-    """
-    if n < 2:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
-
+def file_n_lines(file_name: str, n: int) -> None:
+    with open(file_name, 'r', encoding='utf-8') as file:
+        strings = [string.strip() for string in file.readlines()]
+        for string in strings[:n]:
+            print(string)
 
 if __name__ == '__main__':
-    assert fibonacci(50) == 12586269025
-    assert fibonacci(60) == 1548008755920
-    assert fibonacci(70) == 190392490709135
-    assert fibonacci(80) == 23416728348467685
-    assert fibonacci(90) == 2880067194370816120
-    assert fibonacci(100) == 354224848179261915075
-    assert fibonacci.__name__ == 'fibonacci'
-    assert fibonacci.__doc__.strip() == 'Возвращает n-ое число Фибоначчи'
-    print('Good')
+
