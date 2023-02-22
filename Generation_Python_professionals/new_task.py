629181c057cb29873f50933d5275804ac8c41958
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-22 21:43:50 +0300

    Шея, фея, затея ...
    В этой задаче вам необходимо обработать файл с названием words.txt, содержащий множество неуникальных слов. Само содержимое файла можно посмотреть здесь. Ваша задача найти в нем все слова, заканчивающиеся на строку ЕЯ, и вывести их на экран. При этом нужно:
    
    исключить дубли
    привести все буквы к верхнему регистру
    расположить слова в выводе в порядке двойной сортировки: сперва отсортировать по возрастанию длины слова, а при одинаковых значениях длины расположить по алфавиту

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,10 +1,11 @@
 def find_words(file_name: str) -> dict:
     words = {}
     with open(file_name, 'r', encoding='utf-8') as file:
-        for word in [word.upper() for word in file.read().split()]:
+        for word in [word.upper() for word in file.read().split() if word.upper()[-2:] == 'ЕЯ']:
             words[word] = words.get(word, 0) + 1
-        return words
+
+    return sorted(words, key=lambda x: (len(x), x))
 
 
 if __name__ == '__main__':
-    print(find_words('lorem.txt'))
+    print(*find_words('words.txt'), sep='\n')
