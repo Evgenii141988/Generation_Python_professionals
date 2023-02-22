b7ab080f52326121b88dc142b605374902336fd2
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-22 21:31:29 +0300

    В вашем распоряжении имеется файл lorem.txt. Ваша задача посчитать сколько раз встретилось каждое слово в этом тексте. Для этого создайте словарь words, где ключом будет слово, а значением - количество раз появления этого слова в тексте.
    Регистр букв в словах учитывать не нужно, поэтому слова Hello и hEllO являются эквивалентными. Значения ключа в словаре words записывайте в верхнем регистре
    
    Например, если перед вами был бы такой текст:
    
    Привет как дела
    привет хорошо
    то словарь words выглядел бы так
    
    {'ПРИВЕТ': 2, 'КАК': 1, 'ДЕЛА': 1, 'ХОРОШО': 1}

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,7 +1,9 @@
 def find_unic_words(file_name: str) -> int:
+    words = {}
     with open(file_name, 'r', encoding='utf-8') as file:
-        words = set([word.lower() for word in file.read().split()])
-        return len(words)
+        for word in [word.upper() for word in file.read().split()]:
+            words[word] = words.get(word, 0) + 1
+        return words
 
 
 if __name__ == '__main__':
