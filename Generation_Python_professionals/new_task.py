bb0e05d814585156cbc2adc543ae4df19528961b
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-23 22:33:34 +0300

    Перед вами имеется программа, которая десериализует JSON строку к питоновскому значению
    Сама JSON строка оформлена неправильно, поэтому в программе возникает ошибка json.decoder.JSONDecodeError
    
    Ваша задача найти и исправить ошибки в оформлении  JSON строки. Остальную часть программы не нужно менять

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,7 +1,27 @@
-from string import ascii_lowercase
 import json
 
 if __name__ == '__main__':
-    alphabet = {lit: i for i, lit in enumerate(ascii_lowercase, 1)}
-    json_data = json.dumps(alphabet, indent=3)
-    print(json_data)
+    json_string = """
+    {
+        "customers": [
+            {
+                "userid": 123456,
+                "username": "Allie Hsu",
+                "phone": [
+                    "000-001-0002",
+                    "000-002-0002"
+                ],
+                "is_vip": true
+            },
+            {
+                "userid": 223678,
+                "username": "Donald Duck",
+                "phone": null,
+                "is_vip": false
+            }
+        ]
+    }
+    """
+
+    data = json.loads(json_string)
+    print(data['customers'][0]['username'])
