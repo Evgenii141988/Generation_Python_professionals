42369a18aba54992019daca3e251102daf7a4165
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-23 22:23:45 +0300

    Сериализация словаря Ваша задача выполнить сериализацию словаря из данного задания и сохранить полученную json-строку в переменную json_data
    В качестве ответа распечатайте значение переменной json_data

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,11 +1,7 @@
-def find_words(file_name: str) -> dict:
-    words = {}
-    with open(file_name, 'r', encoding='utf-8') as file:
-        for word in [word.upper() for word in file.read().split() if word.upper()[-2:] == 'ЕЯ']:
-            words[word] = words.get(word, 0) + 1
-
-    return sorted(words, key=lambda x: (len(x), x))
-
+from string import ascii_lowercase
+import json
 
 if __name__ == '__main__':
-    print(*find_words('words.txt'), sep='\n')
+    alphabet = {lit: i for i, lit in enumerate(ascii_lowercase, 1)}
+    json_data = json.dumps(alphabet, indent=3)
+    print(json_data)
