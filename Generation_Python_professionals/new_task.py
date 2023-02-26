98ef39b025701a1052bfb83be07217c3bf3c2a86
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-26 14:51:35 +0300

    Копия range Ваша задача создать функцию-генератор my_range_gen, которая копирует работу range.
    my_range_gen можно запускать, передав ей один параметр stop
    my_range_gen(stop)
    и она должна генерировать последовательность от 0 до stop не включительно
    for i in my_range_gen(5):
        print(i)
    
    # Будет напечатано
    # 0
    # 1
    # 2
    # 3
    # 4
     my_range_gen можно запускать, передав ей два параметра start и stop
    
    my_range_gen(start, stop)
    и она должна генерировать последовательность от start включительно до stop не включительно
    
    for i in my_range_gen(4, 8):
        print(i)
    
    # Будет напечатано
    # 4
    # 5
    # 6
    # 7
     my_range_gen можно запускать, передав ей три параметра start, stop и step
    
    my_range_gen(start, stop, step)
    и она должна генерировать последовательность от start включительно до stop не включительно c шагом step
    
    for i in my_range_gen(4, 8, 2):
        print(i)
    
    # Будет напечатано
    # 4
    # 6
    
    for i in my_range_gen(8, 5, -1):
        print(i)
    
    # Будет напечатано
    # 8
    # 7
    # 6
    предусмотрите вариант запуска my_range_gen со значением step=0. При таком варианте вызова, функция не должна генерировать ни одной последовательности и закончить свою работу. Такое же поведение должно быть если переданы нелогичные значения start, stop и step(см. примеры)
    for i in my_range_gen(4, 8, 0):
        print(i)
    # Ничего не печатает
    
    for i in my_range_gen(20, 10, 3):
        print(i)
    # Ничего не печатает, потому что нельзя пройти от 20 до 10 с шагом 3
    
    for i in my_range_gen(1, 10, -2):
        print(i)
    # Ничего не печатает, потому что нельзя пройти от 1 до 10 с шагом -2

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,15 +1,53 @@
 from typing import Generator
 
 
-def gen_fibonacci_numbers(n: int) -> Generator[int, int, None]:
-    num1 = 0
-    num2 = 1
-    for _ in range(n):
-        num1 += num2
-        yield num2
-        num1, num2 = num2, num1
+def my_range_gen(*args):
+    match args:
+        case int(stop), :
+            step = 1
+            start = 0
+            while start < stop:
+                yield start
+                start += step
+        case int(start), int(stop):
+            step = 1
+            while start < stop:
+                yield start
+                start += step
+        case int(start), int(stop), int(step) if (start < stop and step > 0):
+            while start < stop:
+                yield start
+                start += step
+        case int(start), int(stop), int(step) if (start > stop and step < 0):
+            while start > stop:
+                yield start
+                start += step
+        case _:
+            pass
 
 
 if __name__ == '__main__':
-    for i in gen_fibonacci_numbers(10):
+    # for i in my_range_gen(5):
+    #     print(i)
+    # print()
+    # for i in my_range_gen(4, 8):
+        # print(i)
+    # print()
+    # for i in my_range_gen(4, 8, 2):
+        # print(i)
+    # print()
+    for i in my_range_gen(8, 5, -1):
         print(i)
+    # print()
+    # for i in my_range_gen(4, 8, 0):
+    #     print(i)
+    #
+    # for i in my_range_gen(20, 10, 3):
+    #     print(i)
+    # # Ничего не печатает, потому что нельзя пройти от 20 до 10 с шагом 3
+    #
+    # for i in my_range_gen(1, 10, -2):
+    #     print(i)
+    # for i in range(30, 1, 0):
+    #     print(i)
+
