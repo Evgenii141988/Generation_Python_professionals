f29bf5413d7911630ea5975e6131f1e92671d474
Evgenii Zalesski <evgenii889111@gmail.com>
2023-02-27 21:49:06 +0300

    Перед вами имеется список numbers, состоящий из целых чисел. Ваша задача увеличить каждый элемент списка numbers втрое и сохранить полученный результат в новый список increase_3. Для преобразования используйте функцию map
    В качестве ответа выведите переменную increase_3

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,49 +1,6 @@
-from typing import Generator
-
-
-def my_range_gen(*args):
-    match args:
-        case int(stop), :
-            step = 1
-            start = 0
-        case int(start), int(stop):
-            step = 1
-        case int(start), int(stop), int(step) if (start < stop and step > 0):
-            pass
-        case int(start), int(stop), int(step) if (start > stop and step < 0):
-            while start > stop:
-                yield start
-                start += step
-            return
-        case _:
-            return
-    while start < stop:
-        yield start
-        start += step
-
-
 if __name__ == '__main__':
-    # for i in my_range_gen(5):
-        # print(i)
-    # print()
-    # for i in my_range_gen(4, 8):
-        # print(i)
-    # print()
-    # for i in my_range_gen(4, 8, 2):
-        # print(i)
-    # print()
-    for i in my_range_gen(8, 5, -1):
-        print(i)
-    # print()
-    # for i in my_range_gen(4, 8, 0):
-        # print(i)
-
-    # for i in my_range_gen(20, 10, 3):
-        # print(i)
-    # Ничего не печатает, потому что нельзя пройти от 20 до 10 с шагом 3
-
-    # for i in my_range_gen(1, 10, -2):
-    #     print(i)
-    # for i in range(30, 1, 0):
-    #     print(i)
-
+    numbers = [116, -411, 448, 636, -254, -295, 220, 216, 187, -150, -458, -372, 450, 141, -626, -168, -383, 389, -184,
+               609, 221, 311, 526, 254, -631, -174, -555, -338, 226, 695, -16, 333, 12, -600, -258, -383, -101, 121, 40,
+               278, 118, -462, -671, 78, -69, -568, -228, -445, -47, -565]
+    increase_3 = list(map(lambda x: x * 3, numbers))
+    print(increase_3)
