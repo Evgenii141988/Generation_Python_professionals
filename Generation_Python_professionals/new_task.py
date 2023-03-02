ca42a061b5072f3994561b5c311921eab898b553
Evgenii Zalesski <evgenii889111@gmail.com>
2023-03-02 21:33:27 +0300

    Ваша задача написать функцию find_keys, которая принимает произвольное количество именованных аргументов. Функция должна отобрать только те имена параметров, у которых значения являются списками или кортежами. Функция find_keys должна собрать все имена таких параметров в список, отсортировать их по алфавиту вне зависимости от регистра букв и вернуть в качестве результата.

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -1,9 +1,10 @@
-def count_strings(*args):
-    return len(list(elm for elm in args if isinstance(elm, str)))
+def find_keys(**kwargs):
+    return sorted((key for key, value in kwargs.items() if isinstance(value, (list, tuple))), key=str.lower)
 
 
 if __name__ == '__main__':
-    print(count_strings(1, 2, 'hello', [2, 3, 4], True))
-    print(count_strings('am', 'world', 'hello', 'is'))
-    print(count_strings())
-    print(count_strings(True, False))
+    print(find_keys(t=[4, 5], W=[5, 3], A=(3, 2), a={2, 3}, b=[4]))
+
+    print(find_keys(name='Bruce', surname='Wayne'))
+
+    print(find_keys(marks=[4, 5], name='ashle', surname='Brown', age=20, Also=(1, 2)))
