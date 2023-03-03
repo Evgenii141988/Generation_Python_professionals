ddb6e3d9ad52f6aab61043b98305ff855ae245e4
Evgenii Zalesski <evgenii889111@gmail.com>
2023-03-03 18:41:28 +0300

    Программе на вход поступают слова, разделенные пробелом. Ваша задача проверить, во всех ли словах есть английская буква A вне зависимости от регистра. В качестве ответа программа должна вывести True или False.

--- Generation_Python_professionals/new_task.py
+++ Generation_Python_professionals/new_task.py
@@ -3,8 +3,5 @@ def find_keys(**kwargs):
 
 
 if __name__ == '__main__':
-    print(find_keys(t=[4, 5], W=[5, 3], A=(3, 2), a={2, 3}, b=[4]))
-
-    print(find_keys(name='Bruce', surname='Wayne'))
-
-    print(find_keys(marks=[4, 5], name='ashle', surname='Brown', age=20, Also=(1, 2)))
+    words = input().split()
+    print(all(['a' in word.lower() for word in words]))
