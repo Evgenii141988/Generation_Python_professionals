fe6e678b20308919f096675829e9ce5b4c2fab56
Evgenii Zalesski <evgenii889111@gmail.com>
2023-04-13 18:45:55 +0300

    Подвиг 7. В программе формируется список из введенных городов следующим образом:
    lst_in = list(map(str.strip, input().split()))
    cities = lst_in[:]
    В общем случае полагаем длину списка n элементов. Удалите последний элемент из списка cities и выведите на экран (в консоль) число операций с позиции Big O, необходимых для удаления последнего элемента.
    
    P.S. Вывод нужно делать в формате: 1 - для O(1); n - для O(n); log n - для O(log n); n^2 - для O(n^2); n! - для O(n!).

--- Generation_Python_professionals/data_structure.py
+++ Generation_Python_professionals/data_structure.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
-    usd_rub1 = [68.5, 69.1, 70.2, 71, 69.54]
-    usd_rub2 = [60.1, 62.7, 62.5, 65.6]
-    usd_rub = usd_rub2 + usd_rub1
-    print("n+m")
+    lst_in = list(map(str.strip, input().split()))
+    cities = lst_in[:]
+    d = cities.pop()
+    print(d)
+    print(1)
