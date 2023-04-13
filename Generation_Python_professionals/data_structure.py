fa5eec0c82c071fe6049bb2514e6ef938c2ac682
Evgenii Zalesski <evgenii889111@gmail.com>
2023-04-13 18:49:41 +0300

    Подвиг 8. В программе формируется список из введенных городов следующим образом:
    lst_in = list(map(str.strip, input().split()))
    cities = lst_in[:]
    В общем случае полагаем длину списка n элементов. После этого удалите из этого списка строку "Пермь" (первое вхождение), если такое значение в списке присутствует. Выведите на экран (в консоль) число операций с позиции Big O, необходимых для удаления этого элемента.
    
    P.S. Вывод нужно делать в формате: 1 - для O(1); n - для O(n); log n - для O(log n); n^2 - для O(n^2); n! - для O(n!).

--- Generation_Python_professionals/data_structure.py
+++ Generation_Python_professionals/data_structure.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     lst_in = list(map(str.strip, input().split()))
     cities = lst_in[:]
-    d = cities.pop()
-    print(d)
-    print(1)
+    if "Пермь" in cities:
+        cities.remove("Пермь")
+    print("n")
