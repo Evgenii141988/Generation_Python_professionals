b232f2bb3cce6d9023a2c3ebd1528cebf79b1f1d
Evgenii Zalesski <evgenii889111@gmail.com>
2023-05-01 12:00:57 +0300

    Введите с клавиатуры список с различными значениями (цифры, слова, символы). Необходимо проверить, есть ли в этом списке два слова подряд и вывести их на экран. Если таких пар нет, то выведите фразу “Мало слов!”.

--- Generation_Python_professionals/data_structure.py
+++ Generation_Python_professionals/data_structure.py
@@ -1,6 +1,13 @@
+def solution():
+    x = input().split()
+    n = 0
+    for i in range(1, len(x)):
+        if x[i].isalpha() and x[i - 1].isalpha():
+            print(x[i - 1], x[i])
+            n += 1
+    if n == 0:
+        print("Мало слов!")
+
+
 if __name__ == '__main__':
-    lst_in = list(map(str.strip, input().split()))
-    cities = lst_in[:]
-    if "Пермь" in cities:
-        cities.remove("Пермь")
-    print("n")
+    solution()
