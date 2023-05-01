0b8cc73d0d3ce2600f7010bdf10052959162dbbe
Evgenii Zalesski <evgenii889111@gmail.com>
2023-05-01 12:08:18 +0300

    Напишите функцию с названием detect_lucky, которая принимает в качестве аргумента шестизначное число и возвращает True, если оно является счастливым, и False в противном случае. Счастливым называется шестизначное число, сумма первых трех цифр которого равна сумме последних трех цифр.
    Например, число 237831 является счастливым: 2+3+7 = 12 и 8+3+1 = 12. А 12 =12, поэтому функция вернет True.
    
    Вызов функции уже прописан. Вам остается написать только саму функцию.
    
    Примечание. Код должен возвращать значение, а не печатать его на экран.

--- Generation_Python_professionals/data_structure.py
+++ Generation_Python_professionals/data_structure.py
@@ -1,13 +1,5 @@
-def solution():
-    x = input().split()
-    n = 0
-    for i in range(1, len(x)):
-        if x[i].isalpha() and x[i - 1].isalpha():
-            print(x[i - 1], x[i])
-            n += 1
-    if n == 0:
-        print("Мало слов!")
-
+def detect_lucky(number: int) -> bool:
+    return sum(map(int, str(number)[:3])) == sum(map(int, str(number)[3:]))
 
 if __name__ == '__main__':
-    solution()
+    print(detect_lucky(985778))
